f7c15a9d064adb34fb3f00c5cd441b6753e3417a
1000454 <1000454@SKP1000454PN006.SKP.AD>
2019-03-22 08:45:33 +0900

    day input

--- src/jhest/calendar/Calendar.java
+++ src/jhest/calendar/Calendar.java
@@ -1,48 +1,64 @@
 package jhest.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int maxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else
+			return false;
 	}
 
-	public static void main(String[] args) {
+	public int parseDay(String dayStr) {
+		if (dayStr.equals("SU"))
+			return 0;
+		else if (dayStr.equals("MO"))
+			return 1;
+		else if (dayStr.equals("TU"))
+			return 2;
+		else if (dayStr.equals("WE"))
+			return 3;
+		else if (dayStr.equals("TH"))
+			return 4;
+		else if (dayStr.equals("FR"))
+			return 5;
+		else if (dayStr.equals("SA"))
+			return 6;
+		else
+			return 0;
+	}
 
-//		System.out.println("hello, Calendar");
-//		
-//		System.out.println(" 일  월   화   수  목   금   토");
-//		System.out.println("---------------------");
+	public int maxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else
+			return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar(int year, int month, int day) {
+
+		System.out.printf("\n     [%4d년%3d월]\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+
+		for (int i = 0; i < day; i++) {
+			System.out.print("   ");
+		}
+
+		int maxDay = maxDaysOfMonth(year, month);
+
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if ((i + day) % 7 == 0) {
+				System.out.println();
+			}
+		}
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
-
-		String PROMPT = "cal> ";
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-
-//		System.out.println("반복 횟수를 입력하세요.");
-//		int repeat = scanner.nextInt();
-
-		while (true) {
-			System.out.println("\n달을 입력하세요.");
-			System.out.print(PROMPT);
-			int month = scanner.nextInt();
-			if (month == -1) {
-				break;
-			}
-			if (month > 12) {
-				continue;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxDaysOfMonth(month));
-		}
-
-		System.out.println("\n끝");
-		scanner.close();
-
 	}
 }
---
+++ src/jhest/calendar/Prompt.java
@@ -0,0 +1,49 @@
+package jhest.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+//	private final static String PROMPT = "cal> ";
+
+	public void runPrompt() {
+
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		while (true) {
+
+			System.out.println("\n\n연도를 입력하세요(종료: -1).");
+			System.out.print("YEAR> ");
+			int year = scanner.nextInt();
+			if (year == -1) {
+				break;
+			}
+
+			System.out.println("\n월을 입력하세요.");
+			System.out.print("MONTH> ");
+			int month = scanner.nextInt();
+
+			System.out.println("\n첫번째 요일을 입력하세요.");
+			System.out.print("DAY> ");
+			String dayStr = scanner.next();
+			int day = cal.parseDay(dayStr);
+
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+//			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxDaysOfMonth(month));
+			cal.printSampleCalendar(year, month, day);
+		}
+
+		System.out.println("\n끝");
+		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+
+}
