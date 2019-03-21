95c88733a1fb6aa265409018c97d6c3d9e240ec5
1000454 <1000454@SKP1000454PN006.SKP.AD>
2019-03-21 16:15:39 +0900

    repeat

--- src/jhest/calendar/Calendar.java
+++ src/jhest/calendar/Calendar.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int maxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	
+
 	public static void main(String[] args) {
 
 //		System.out.println("hello, Calendar");
@@ -21,16 +21,20 @@ public class Calendar {
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
 
-			
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
-		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.maxDaysOfMonth(month));
 
+		System.out.println("반복 횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요.");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cal.maxDaysOfMonth(month));
+		}
+
+		System.out.println("끝");
 		scanner.close();
-		
+
 	}
 }
