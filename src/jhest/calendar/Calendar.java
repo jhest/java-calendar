7e786797322b51da96c306f3bb7821041f14a1fa
1000454 <1000454@SKP1000454PN006.SKP.AD>
2019-03-21 16:08:09 +0900

    max days

--- src/jhest/calendar/Calendar.java
+++ src/jhest/calendar/Calendar.java
@@ -1,17 +1,36 @@
 package jhest.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int maxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("hello, Calendar");
+
+//		System.out.println("hello, Calendar");
+//		
+//		System.out.println(" 일  월   화   수  목   금   토");
+//		System.out.println("---------------------");
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+
+			
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		
-		System.out.println(" 일  월   화   수  목   금   토");
-		System.out.println("---------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt();
 		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.maxDaysOfMonth(month));
+
+		scanner.close();
 		
 	}
 }
